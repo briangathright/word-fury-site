0cfbc15587d54f17e629e50b02452d4628336da6
cheerguyme <utchrisosu@gmail.com>
2015-04-28 14:14:24 -0500

    updated game on site repo

--- game/src/Game.js
+++ game/src/Game.js
@@ -1,125 +1,197 @@
-BasicGame.Game = function (game) {  
-    this.word2 = null;
-    this.wordSprite = null;
-    this.wordList;
-    this.entry = null;
-    this.scoreText = null;
-    this.score = 0;
+/**
+ This file is part of WordFury.
+
+    WordFury is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WordFury is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WordFury.  If not, see <http://www.gnu.org/licenses/>.
+    */
+WordFury.Game = function(game){
+    // define variables just for WordFury.Game   
+    this._entry = null;
+    this._spawnWordTimer = 0;
+    this._wordGroup = null;
+    // define variables that can be in WordFury.item
+    WordFury._scoreText = null;
+    WordFury._score = 0;
+    WordFury._wordList = null;
+    WordFury._fontStyle = null;
+    WordFury._wordCount = 0;
+    WordFury._spawnedWordCount = 0;
 };
+WordFury.Game.prototype = {
+    create: function(){
+        // start the physics engine
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        //this.physics.arcade.gravity.y = 1;
+        WordFury.MUSIC.play('', 0, 1, true);
 
-BasicGame.Game.prototype = {
-
-    create: function () {
-        this.wordList = this.cache.getJSON('wordList');
+        WordFury.MUSIC.onLoop.add(this.playLevelMusic, this);
+        // set background
+        var background = this.add.sprite(0, 0, 'preloaderBackground');
+        background = this.add.tileSprite(0, 0, 640, 960, 'preloaderBackground');
+        background.autoScroll(-200, 0);
+        // load the wordList
+        WordFury._wordList = this.cache.getJSON('wordList');
+        // create the textBox
         var textInputCanvas = document.getElementById('canvas');
-        textInputCanvas.style.top = 700 + 'px';
-        textInputCanvas.style.left = this.world.centerX - textInputCanvas.width/2 + 'px';
-        this.scoreText = this.game.add.text(this.world.leftX, 700, 'Score: ' + this.score, {
-            font: "32px Arial",
-            fill: "#ff0044",
-            align: "center"
-        });
-        //this.scoreText.anchor.setTo(0.5, 0.5);
-        this.entry = new CanvasInput({
+        // textInputCanvas.style.top = this.world.botY + 100 + 'px';
+        // textInputCanvas.style.left = this.stage.offset.x * 1.5 - 150 + 'px';
+        textInputCanvas.style.top = 90 + "%";
+        textInputCanvas.style.left = 40 + "%";
+        textInputCanvas.style.width = 40 + "%";
+        textInputCanvas.style.height = 15 + "%";
+        
+       
+        this._entry = new CanvasInput({
             canvas: document.getElementById('canvas'),
-            //canvas: document.getElementById(this.game.canvas.id);
             fontSize: 18,
             fontFamily: 'Arial',
             fontColor: '#212121',
             fontWeight: 'bold',
-            width: 300,
+            width: this.width / window.innerWidth,
             padding: 8,
             borderWidth: 1,
             borderColor: '#000',
             borderRadius: 3,
             boxShadow: '1px 1px 0px #fff',
             innerShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)',
-            placeHolder: 'Enter message here...'
+            placeHolder: 'Enter word here...'
         });
-        this.entry.focus();
-
+        this._entry.focus();
         var that = this;
-        this.entry.onsubmit(
+        this._entry.onsubmit(
             function() {
-                that.wordSubmit();
+                that.wordSubmit(that);
             }
         );
-        
+        // set font style
+        WordFury._fontStyle = { font: "32px Arial", fill: "#ff0044", align: "center" };
+        // initialize spawn timer
+        this._spawnWordTimer = 0;
+        // initialize score text with value 0
+        WordFury._scoreText = this.add.text(this.world.leftX, 700, 'Score: 0', WordFury._fontStyle);            
+        // create new group for words
+        this._wordGroup = this.add.group();
+        // spawn first word
+        WordFury.item.spawnWord(this);
+    },
+    wordSubmit: function(game){
+        this.checkValue(game);
+    },
+    checkValue: function(game){
+        var enteredWord = this._entry.value();
+        this._entry.value("");
 
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        //this.word2 = this.game.add.bitmapText(this.world.centerX, this.world.topY, 'stack', 'apple', 64);        
-
-        this.word2 = this.game.add.text(0, 0, this.rnd.pick(this.wordList), {
-            font: "32px Arial",
-            fill: "#ff0044",
-            align: "center"
+        this._wordGroup.forEachAlive(function(wordSprite){
+            var word = wordSprite.getChildAt(0);
+            if(enteredWord.trim() == word.text.trim()) {
+                WordFury._wordCount ++;
+                game.updateScore();
+                wordSprite.kill();
+                WordFury._spawnedWordCount += 1;
+            }
         });
-
-        this.word2.anchor.setTo(0.5, 0.5);
-        //this.word2.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
-        
-        this.wordSprite = this.add.sprite(this.world.centerX, this.world.topY, null);
-        this.wordSprite.addChild(this.word2);
-
-        this.game.physics.enable(this.wordSprite);
-        this.wordSprite.body.velocity.setTo(0, 100);
     },
+    playLevelMusic:function(){
+            WordFury.MUSIC.play('', 0, 1, true);
 
-    wordSubmit: function () {
-        this.checkValue();
-    },
 
-    checkValue: function() {
-        var word = this.entry.value();
-
-        this.entry.value("");
-        if(word.trim() == this.word2.text.trim()) {
-            this.removeWord(word);
-            this.updateScore();
-            this.wordSprite.destroy();
-            this.word2 = this.game.add.text(0, 0, this.rnd.pick(this.wordList), {
-            font: "32px Arial",
-            fill: "#ff0044",
-            align: "center"
-        });
-        this.word2.anchor.setTo(0.5);        
-        this.wordSprite = this.add.sprite(this.rnd.realInRange(100, this.world.width-100), this.world.topY, null);
-        this.wordSprite.addChild(this.word2);
-
-        this.game.physics.enable(this.wordSprite);
-        this.wordSprite.anchor.setTo(0.5, 0.5);
-        this.wordSprite.body.velocity.setTo(0, 100);
-        this.wordSprite.body.angularVelocity = 50;
-        }
-    },
-
-    removeWord: function(word) {
-        var index = this.wordList.indexOf(word);
-        if(index > -1) {
-            this.wordList.splice(index, 1);
-        }
-    },
-
+        },
+    
     updateScore: function() {
-        this.score += 10;
-        this.scoreText.text = 'Score: ' + this.score;
-    },
+        WordFury._score += 10;
+        WordFury._scoreText.text = 'Score: ' + WordFury._score;
 
-    update: function () {
-        
-        //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
     },
+    update: function(){
+        // update the timer
+        this._spawnWordTimer += this.time.elapsed;
+      
+        // check to see if we should spawn another word
+        if(this._spawnWordTimer > (2000-(WordFury._wordCount*10))) {
+            // reset the timer
+            this._spawnWordTimer = 0;
+            // spawn a word
+            WordFury.item.spawnWord(this);
+        }
 
+        if (WordFury._spawnedWordCount == 300){//after 280 of the 300 words have spawned call win game
+            this.state.states['GameWin']._score=WordFury._score;
+                this.state.start('GameWin');
+        }
+
+        if (WordFury._score <0){
+            this.state.states['GameOver']._score=WordFury._score;
+            this.state.start('GameOver' );
+        }
+    },
     quitGame: function (pointer) {
-
-        //  Here you should destroy anything you no longer need.
-        //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
-
-        //  Then let's go back to the main menu.
+        WordFury.MUSIC.stop();
+        WordFury._score = 0;
+        // need to figure out how to hide textbox again.
         this.state.start('MainMenu');
-
     }
-
 };
 
+WordFury.item = {
+    spawnWord: function(game){
+        var word = game.add.text(0, 0, game.rnd.pick(WordFury._wordList), WordFury._fontStyle);
+        this.removeWord(word.text);
+        word.anchor.setTo(0.5);        
+        var wordSprite = game.add.sprite(game.rnd.realInRange(100, WordFury.GAME_WIDTH-100), game.world.topY, null);
+        wordSprite.addChild(word);
+        game.physics.enable(wordSprite);
+        wordSprite.anchor.setTo(0.5, 0.5);
+        //wordSprite.body.velocity.setTo(0, 100+WordFury._wordCount*2);
+        wordSprite.body.velocity.setTo(0, 20+WordFury._wordCount*2);
+        var rand = game.rnd.realInRange(0, 100);
+        if(rand > 50) {
+            wordSprite.body.angularVelocity = 50;
+        }
+        else {
+            wordSprite.body.angularVelocity = -50;
+        }
+        wordSprite.checkWorldBounds = true;
+        wordSprite.events.onOutOfBounds.add(this.loseLife,this);
+        game._wordGroup.add(wordSprite);
+    },
+    removeWord: function(word){
+        var index = WordFury._wordList.indexOf(word);
+        if(index > -1) {
+            WordFury._wordList.splice(index, 1);
+        }
+    },
+    loseLife: function(word){
+        WordFury._score -=10;
+        word.kill();
+        WordFury._spawnedWordCount += 1;
+        WordFury._scoreText.text = 'Score: ' + WordFury._score;
+        
+    }
+};
+
+ /**
+ This file is part of WordFury.
+
+    WordFury is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WordFury is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WordFury.  If not, see <http://www.gnu.org/licenses/>.
+    */
